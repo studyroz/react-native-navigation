f8ddd1027ee6669c3b0bfd8c690d793db7db09ea
stephan.zeng <stephan.zeng@dji.com>
2017-11-08 20:43:30 +0800

    fix:解决Android启动立即退出后再启动的问题

--- android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
@@ -2,6 +2,8 @@ package com.reactnativenavigation.controllers;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,20 +13,46 @@ import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.ReactDevPermission;
 
 public abstract class SplashActivity extends AppCompatActivity {
+    private static final int MSG_INIT_REACT_CONTEXT = 2100;
     public static boolean isResumed = false;
+    private Handler mHandler;
+    private boolean mDelayTimeOut = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setSplashLayout();
         IntentDataHandler.saveIntentData(getIntent());
+
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                if(isFinishing()) return;
+                if(msg.what == MSG_INIT_REACT_CONTEXT) {
+                    mDelayTimeOut = true;
+                    if(!hasInitReady()){
+                        return;
+                    }
+                    initReactContext();
+                }
+            }
+        };
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         isResumed = true;
+        if (getDelayFinishTime() > 0) {
+            mDelayTimeOut = false;
+            mHandler.removeMessages(MSG_INIT_REACT_CONTEXT);
+            mHandler.sendEmptyMessageDelayed(MSG_INIT_REACT_CONTEXT, getDelayFinishTime());
+        } else if(hasInitReady()){
+            initReactContext();
+        }
+    }
 
+    private void initReactContext() {
         if (NavigationApplication.instance.getReactGateway().hasStartedCreatingContext()) {
             NavigationApplication.instance.getEventEmitter().sendAppLaunchedEvent();
             if (NavigationApplication.instance.clearHostOnActivityDestroy()) {
@@ -78,5 +106,18 @@ public abstract class SplashActivity extends AppCompatActivity {
         View view = new View(this);
         view.setBackgroundColor(Color.WHITE);
         return view;
+    }
+
+    protected int getDelayFinishTime() {
+        return 0;
+    }
+
+    protected boolean hasInitReady(){
+        return true;
+    }
+
+    protected void onInitReady(){
+        if(!mDelayTimeOut) return;
+        initReactContext();
     }
 }
