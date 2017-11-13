93a613b8261ed999f9dd0ef87f02bcd022008e68
yel.huang <yel.huang@dji.com>
2017-11-13 22:07:14 +0800

    防止在隐藏小红点时闪现大红点

--- android/app/src/main/java/com/reactnativenavigation/layouts/BottomTabsLayout.java
+++ android/app/src/main/java/com/reactnativenavigation/layouts/BottomTabsLayout.java
@@ -532,7 +532,7 @@ public class BottomTabsLayout extends BaseLayout implements AHBottomNavigation.O
         } else {
             bottomTabs.setNotification(badge, index);
             getButtons(bottomTabs, list);
-            if (list.size() >= index) {
+            if (list.size() >= index && !badge.equals("")) {
                 restoreStyle(list.get(index));
             }
         }
@@ -551,7 +551,7 @@ public class BottomTabsLayout extends BaseLayout implements AHBottomNavigation.O
             bottomTabs.setNotification(badge, getScreenStackIndex(navigatorId));
             getButtons(bottomTabs, list);
 
-            if (list.size() >= getScreenStackIndex(navigatorId)) {
+            if (list.size() >= getScreenStackIndex(navigatorId) && !badge.equals("")) {
                 restoreStyle(list.get(getScreenStackIndex(navigatorId)));
             }
         }
