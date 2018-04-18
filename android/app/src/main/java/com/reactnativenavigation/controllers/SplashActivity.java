ac521bb9e17d4522b4648bb6b65fb0ef42e42727
peter.zhang <peter.zhang@dji.com>
2018-04-18 15:43:24 +0800

    Replace SplashActivity with the upstream one.

--- android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,10 +14,7 @@ import com.reactnativenavigation.react.*;
 import com.reactnativenavigation.utils.CompatUtils;
 
 public abstract class SplashActivity extends AppCompatActivity {
-    private static final int MSG_INIT_REACT_CONTEXT = 2100;
     public static boolean isResumed = false;
-    private Handler mHandler;
-    private boolean mDelayTimeOut = false;
 
     public static void start(Activity activity) {
         Intent intent = activity.getPackageManager().getLaunchIntentForPackage(activity.getPackageName());
@@ -38,36 +33,13 @@ public abstract class SplashActivity extends AppCompatActivity {
         LaunchArgs.instance.set(getIntent());
         setSplashLayout();
         IntentDataHandler.saveIntentData(getIntent());
-
-        mHandler = new Handler(){
-            @Override
-            public void handleMessage(Message msg) {
-                if(isFinishing()) return;
-                if(msg.what == MSG_INIT_REACT_CONTEXT) {
-                    mDelayTimeOut = true;
-                    if(!hasInitReady()){
-                        return;
-                    }
-                    initReactContext();
-                }
-            }
-        };
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         isResumed = true;
-        if (getDelayFinishTime() > 0) {
-            mDelayTimeOut = false;
-            mHandler.removeMessages(MSG_INIT_REACT_CONTEXT);
-            mHandler.sendEmptyMessageDelayed(MSG_INIT_REACT_CONTEXT, getDelayFinishTime());
-        } else if(hasInitReady()){
-            initReactContext();
-        }
-    }
 
-    private void initReactContext() {
         if (NavigationApplication.instance.getReactGateway().hasStartedCreatingContext()) {
             if (CompatUtils.isSplashOpenedOverNavigationActivity(this, getIntent())) {
                 finish();
@@ -125,18 +97,5 @@ public abstract class SplashActivity extends AppCompatActivity {
         View view = new View(this);
         view.setBackgroundColor(Color.WHITE);
         return view;
-    }
-
-    protected int getDelayFinishTime() {
-        return 0;
-    }
-
-    protected boolean hasInitReady(){
-        return true;
-    }
-
-    protected void onInitReady(){
-        if(!mDelayTimeOut) return;
-        initReactContext();
     }
 }
