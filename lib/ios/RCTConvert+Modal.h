785b35a66a61c60b2e1123379a43f770243af6c5
AmberShang <48346592+AmberShang@users.noreply.github.com>
2019-09-03 16:49:13 +0800

    Add automatic presentation style (#8)
    * Add automatic presentation style

--- lib/ios/RCTConvert+Modal.h
+++ lib/ios/RCTConvert+Modal.h
@@ -14,15 +14,55 @@ RCT_ENUM_CONVERTER(UIModalTransitionStyle,
 					  }), UIModalTransitionStyleCoverVertical, integerValue)
 
 RCT_ENUM_CONVERTER(UIModalPresentationStyle,
-				   (@{@"fullScreen": @(UIModalPresentationFullScreen),
-					  @"pageSheet": @(UIModalPresentationPageSheet),
-					  @"formSheet": @(UIModalPresentationFormSheet),
-					  @"currentContext": @(UIModalPresentationCurrentContext),
-					  @"custom": @(UIModalPresentationCustom),
-					  @"overFullScreen": @(UIModalPresentationOverFullScreen),
-					  @"overCurrentContext": @(UIModalPresentationOverCurrentContext),
-					  @"popover": @(UIModalPresentationPopover),
-					  @"none": @(UIModalPresentationNone)
-					  }), UIModalPresentationFullScreen, integerValue)
+				   ([self PresentationStyle]), UIModalPresentationFullScreen, integerValue)
+
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >=  130000 // iOS 13以上的系统版本 增加一种modal Automatic样式
++ (NSDictionary *)PresentationStyle {
+    if (@available(iOS 13.0, *)) {
+        return @{
+                @"fullScreen": @(UIModalPresentationFullScreen),
+                @"pageSheet": @(UIModalPresentationPageSheet),
+                @"formSheet": @(UIModalPresentationFormSheet),
+                @"currentContext": @(UIModalPresentationCurrentContext),
+                @"custom": @(UIModalPresentationCustom),
+                @"overFullScreen": @(UIModalPresentationOverFullScreen),
+                @"overCurrentContext": @(UIModalPresentationOverCurrentContext),
+                @"popover": @(UIModalPresentationPopover),
+                @"none": @(UIModalPresentationNone),
+                @"automatic":@(UIModalPresentationAutomatic),
+                };
+    } else {
+        return @{
+                @"fullScreen": @(UIModalPresentationFullScreen),
+                @"pageSheet": @(UIModalPresentationPageSheet),
+                @"formSheet": @(UIModalPresentationFormSheet),
+                @"currentContext": @(UIModalPresentationCurrentContext),
+                @"custom": @(UIModalPresentationCustom),
+                @"overFullScreen": @(UIModalPresentationOverFullScreen),
+                @"overCurrentContext": @(UIModalPresentationOverCurrentContext),
+                @"popover": @(UIModalPresentationPopover),
+                @"none": @(UIModalPresentationNone),
+                @"automatic":@(UIModalPresentationFullScreen),
+                };
+    }
+}
+#else
++ (NSDictionary *)PresentationStyle {
+        return @{
+                @"fullScreen": @(UIModalPresentationFullScreen),
+                @"pageSheet": @(UIModalPresentationPageSheet),
+                @"formSheet": @(UIModalPresentationFormSheet),
+                @"currentContext": @(UIModalPresentationCurrentContext),
+                @"custom": @(UIModalPresentationCustom),
+                @"overFullScreen": @(UIModalPresentationOverFullScreen),
+                @"overCurrentContext": @(UIModalPresentationOverCurrentContext),
+                @"popover": @(UIModalPresentationPopover),
+                @"none": @(UIModalPresentationNone),
+                @"automatic":@(UIModalPresentationFullScreen),
+                };
+}
+#endif
+
 @end
 
